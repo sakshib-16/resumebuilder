2eccce76b5766e3ed2e4ed539cf8ff249cf27ec9
deepanshu188 <deepanshubisht188@gmail.com>
2022-03-15 12:14:34 +0530

    create resume

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Template1 from './Component/Container/Templates/Template1/Template1';
 import { Auth } from './Component/Auth/Auth';
 import { Navigation } from './Component/Container/Navigation/Navigation';
+import { CreateResume } from './Component/Container/CreateResume/CreateResume';
 
 function App() {
   return (
     <div className="App">
       {/* <Template1 /> */}
       <Navigation />
-      <Auth />
+      {/* <Auth /> */}
+      <CreateResume />
     </div>
   );
 }
--- src/Component/Container/CreateResume/CreateResume.js
+++ src/Component/Container/CreateResume/CreateResume.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import classes from './createResume.module.css'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 export const CreateResume = () => {
   return (
-    <div>CreateResume</div>
+    <div className={classes.create_resume}>
+      <h1>Create Resume</h1>
+      <div>
+        <AddCircleOutlineIcon fontSize='large' />
+        <p>Create New</p>
+      </div>
+    </div>
   )
 }
