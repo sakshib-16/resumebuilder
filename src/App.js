cc9aaedd760290981a69a9f7e3c998c6fe2e1eee
gourav jha <gouravjha019@gmai.com>
2022-03-15 12:46:59 +0530

    homepage and sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Template1 from './Component/Templates/Template1/Template1';
+// import Template1 from './Component/Templates/Template1/Template1';
+import Template1 from './Component/Container/Template/Template1/Template1'
+import HomePage from './Component/Container/Homepage/Homepage'
+import { Sidebar } from './Component/Container/Details/Sidebar/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
-      <Template1/>
+      {/* <Template1/> */}
+      {/* <HomePage/> */}
+      {/* <Sidebar/> */}
     </div>
   );
 }
