82c795be8b7200c4c07d6b149559b135c37dd904
deepanshu188 <deepanshubisht188@gmail.com>
2022-03-16 09:51:45 +0530

    create resume UI created

--- src/Component/Container/CreateResume/CreateResume.js
+++ src/Component/Container/CreateResume/CreateResume.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import classes from './createResume.module.css'
+import classes from './resume.module.css'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { MyResume } from './MyResume';
 
 export const CreateResume = () => {
   return (
-    <div className={classes.create_resume}>
+    <div>
+    <div className={`${classes.create_resume} ${classes.resume}`} >
       <h1>Create Resume</h1>
       <div>
         <AddCircleOutlineIcon fontSize='large' />
         <p>Create New</p>
       </div>
+    </div>
+    <MyResume />
     </div>
   )
 }
