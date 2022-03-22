ec53694d5d90eb9a30dc0ae9b316cce0e0045673
deepanshu188 <deepanshubisht188@gmail.com>
2022-03-22 14:51:19 +0530

    changes in login and signup

--- src/Component/Auth/Login.js
+++ src/Component/Auth/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Form } from './Form'
 import classes from './auth.module.css'
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useDispatch, useSelector } from 'react-redux';
+import HomePage from '../Container/Homepage/Homepage';
+import { menu } from '../../Redux/action';
 
 // const auth = getAuth();
 // signInWithEmailAndPassword(auth, email, password)
@@ -18,17 +21,37 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 //   });
 
 export const Login = ({btn, msg}) => {
+  const dispatch = useDispatch()
+  const menu = useSelector(i => i.menuReducer)
+  const credential = useSelector((i) => i.authReducer)
+
+  const auth = getAuth();
+signInWithEmailAndPassword(auth, credential.email, credential.password)
+.then((userCredential) => {
+ // Signed in 
+ const user = userCredential.user;
+ dispatch(menu(true))
+ console.log(user)
+
+ // ...
+})
+.catch((error) => {
+ const errorCode = error.code;
+ const errorMessage = error.message;
+});
+
+  console.log(menu)
   return (
-  <div className={classes.login}>
-    <div className={classes.banner}>
+    <div className={classes.login}>
+      <div className={classes.banner}>
+      </div>
+        <div>
+        <Form btn={btn} msg={msg} />
+        <div className={classes.signup_msg}>
+        <p>Dont have an account?</p>
+        <Link to="/signup"><a href="#">Sign up now</a></Link>
+        </div>
+      </div>
     </div>
-    <div>
-     <Form btn={btn} msg={msg} />
-     <div className={classes.signup_msg}>
-       <p>Dont have an account?</p>
-     <Link to="/signup"><a href="#">Sign up now</a></Link>
-     </div>
-    </div>
-  </div>
   )
 }
